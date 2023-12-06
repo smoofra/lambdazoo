c33e16549d95a55352292a4b77edd17f36765adc
Lawrence D'Anna <larry@elder-gods.org>
2023-12-06 14:09:05 -0500

    strictEqual
    Switched to assert.strictEqual, aparently equal is deprecatd.

--- test.spec.ts
+++ test.spec.ts
@@ -1,12 +1,12 @@
-import { equal } from "assert";
+import { strictEqual } from "assert";
 import {expectEOF} from "typescript-parsec"
 import {TERM, tokenizer, termTree, Term} from "./hello"
 
 function parse(s : string) : Term {
     const r = expectEOF(TERM.parse(tokenizer.parse(s)));
-    equal (r.successful, true)
+    strictEqual (r.successful, true)
     if (r.successful) {
-        equal(r.candidates.length, 1)
+        strictEqual(r.candidates.length, 1)
         return r.candidates[0].result;
     }
     throw new Error("failed.")
@@ -14,9 +14,9 @@ function parse(s : string) : Term {
 
 describe("lambdazoo tests", () => {
   it("should associate application to the left", () => {
-    equal(termTree(parse("FOO BAR BAZ QUUX")), "(((FOO BAR) BAZ) QUUX)");
+    strictEqual(termTree(parse("FOO BAR BAZ QUUX")), "(((FOO BAR) BAZ) QUUX)");
   });
   it("should bind calls stronger than lambdas", () => {
-    equal(termTree(parse("λ x → x x")), "(λx→(x x))")
+    strictEqual(termTree(parse("λ x → x x")), "(λx→(x x))")
   });
 });
