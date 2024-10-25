846d0d6fbc0fcef10d297770254fc8e5966803ba
Lawrence D'Anna <larry@elder-gods.org>
2024-10-25 08:51:47 -0400

    switch to @std/assert

--- lambda_test.ts
+++ lambda_test.ts
@@ -1,13 +1,13 @@
 
-import { strictEqual } from "node:assert";
+import { assertEquals } from "@std/assert";
 import {termTree, toJavascript, parseTerm, parseToplevel} from "./lambda.ts"
 
 Deno.test("left association", () => {
-    strictEqual(termTree(parseTerm("FOO BAR BAZ QUUX")), "(((FOO BAR) BAZ) QUUX)");
+    assertEquals(termTree(parseTerm("FOO BAR BAZ QUUX")), "(((FOO BAR) BAZ) QUUX)");
 })
 
 Deno.test("bind calls stronger than lambda", () => {
-    strictEqual(termTree(parseTerm("λ x . x x")), "(λx.(x x))")
+    assertEquals(termTree(parseTerm("λ x . x x")), "(λx.(x x))")
 })
 
 
@@ -18,15 +18,15 @@ meta = λx. x x
 
 Deno.test("parse toplevel declarations", () => {
     const [id, meta] = parseToplevel(src)
-    strictEqual(id.name, "id")
-    strictEqual(termTree(id.term), "(λx.x)")
-    strictEqual(meta.name, "meta")
-    strictEqual(termTree(meta.term), "(λx.(x x))")
+    assertEquals(id.name, "id")
+    assertEquals(termTree(id.term), "(λx.x)")
+    assertEquals(meta.name, "meta")
+    assertEquals(termTree(meta.term), "(λx.(x x))")
 })
 
 Deno.test("translate to javascript", () => {
     const two = eval(toJavascript(parseTerm("λx. λ y . x (x y)")))
     const inc = (x:number) => x + 1
-    strictEqual(two(inc)(0), 2)
-    strictEqual(two(inc)(1), 3)
+    assertEquals(two(inc)(0), 2)
+    assertEquals(two(inc)(1), 3)
 })
